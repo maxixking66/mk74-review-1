65212e7eebe9cf00bfb2684702c1bca403f1bed1
maxixking66 <maxixking66@gamil.com>
2022-05-26 12:31:00 +0430

    add DataSource

---
+++ src/com/maktabsharif74/review/util/DataSource.java
@@ -0,0 +1,24 @@
+package com.maktabsharif74.review.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DataSource {
+
+    private final String username = "root";
+    private final String password = "123456";
+    private final String url = "jdbc:mysql://localhost:3306";
+    private final Connection connection;
+
+    public DataSource() throws SQLException {
+        this.connection =
+                DriverManager.getConnection(
+                        url, username, password
+                );
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+}
